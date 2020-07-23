11ae6d140a6421d21c11d47382c4e0a9dbd7b6ce
ADI-KOTKAR <adityakotkar75@gmail.com>
2020-07-23 21:08:22 +0530

    events added-jobs,internships

---
+++ Website_Crawlers/Events/Internships/Internships.py
@@ -0,0 +1,61 @@
+# Import
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC
+
+import csv
+
+''' Code '''
+PATH = 'C:\Program Files (x86)/chromedriver.exe'
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://www.timesjobs.com/candidate/job-search.html?from=submit&actualTxtKeywords=Internship&searchBy=0&rdoOperator=OR&searchType=personalizedSearch&luceneResultSize=25&postWeek=60&txtKeywords=Internship&pDate=I&sequence=9&startPage=8")
+#print(driver.title)
+data = []
+
+''' Functions '''
+def extract():
+    try: 
+        ul = WebDriverWait(driver, 10).until( 
+            EC.presence_of_element_located((By.CLASS_NAME,"new-joblist")) 
+        )
+        #print(ul.text)
+        lists = ul.find_elements_by_class_name('job-bx')
+        #print(lists.text)
+        for li in lists:
+            print("---------------")
+            title = li.find_element_by_tag_name("h2")
+            print('Title: ',title.text)
+            data.append([title.text, "Internships"])
+        next_page()
+    except: 
+        driver.quit()
+
+def next_page():
+    try: 
+        next_page = WebDriverWait(driver, 10).until( 
+            EC.presence_of_element_located((By.CLASS_NAME, "nxtC")) 
+        ) 
+        next_button = next_page.find_element_by_tag_name("a")
+        next_button.send_keys(Keys.ENTER)
+        extract()
+    except: 
+        driver.quit() 
+
+''' CSV creation '''
+extract()
+with open('Internships.csv','a+', newline='') as fp:
+    thewriter = csv.writer(fp)
+    thewriter.writerows(data)
+
+time.sleep(10)
+driver.quit()
+# print(driver.page_source)
+
+
+
+
